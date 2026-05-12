55da20a630873b893d024fd70455b3bc6257d67c
TFolkner <markov.ilya.work@gmail.com>
2026-05-12 19:50:10 +0300

    сохранён формат исходных значений в расширенной выборке + правки графиков

--- matlab/main.m
+++ matlab/main.m
@@ -4,9 +4,14 @@ clc
 %% macros =================================================================
 macros.make_demo = 0;
 
+
+
 %% init data ============================================================== 
 data_folder = "../run/"; 
-sFile_data_sample_xls = data_folder + "sample_data.xlsx"; 
+sFile_data_sample_xls = data_folder + "main_diploma_data.xlsx"; 
+
+
+
 
 %% make test sample ======================================================= 
 if (macros.make_demo == 1) 
@@ -47,14 +52,14 @@ end
 %% load and struct data =================================================== 
 try 
     pFile_data = importdata (sFile_data_sample_xls); 
-    clear sFile_data_sample_xls data_folder 
+    clear sFile_data_sample_xls 
     
     fprintf ("[INFO] -- %s\n", "table load and saved"); 
 catch exception 
     fprintf ("[ERROR] -- %s\n", exception.message); 
 end 
 
-%% expansion sample ======================================================= 
+%% expansion data ========================================================= 
 
 work_data = struct; 
 work_data.default_data = pFile_data; 
@@ -64,12 +69,13 @@ default_ordinat = 1:1:work_data.default_result(1);
 new_ordinatre = linspace (1, work_data.default_result(1), work_data.expansion_result); 
 work_data.new_data = zeros (work_data.expansion_result , work_data.default_result (2)); 
 
-for i = 1:work_data.default_result (2) 
-    work_data.new_data(:, i) = int64 (interp1 (default_ordinat, pFile_data(:, i), new_ordinatre, 'spline')); 
+for i = 1:work_data.default_result (2)
+    if (i == 9 || i == 10 || i == 11 || i == 12)
+        work_data.new_data(:, i) = round(interp1 (default_ordinat, pFile_data(:, i), new_ordinatre, 'spline'), 1);
+    else
+        work_data.new_data(:, i) = int64 (interp1 (default_ordinat, pFile_data(:, i), new_ordinatre, 'spline')); 
+    end
 end
-
-
-
 
 % shuffle data
 for i = 1:work_data.default_result (2) 
@@ -77,27 +83,28 @@ for i = 1:work_data.default_result (2)
     work_data.new_data_shuffle(:, i) = to_shuffle(randperm(length(to_shuffle)));
 end
 
+% save data to xlsx
+table2save = array2table (work_data.new_data_shuffle); 
+folder2save = data_folder+"expansion_data.xlsx"; 
+writetable (table2save, folder2save); 
+fprintf ("[INFO] -- %s\n", "expansion table created and saved");
 
 %% plot
-% плотность частот
-% to_plot = work_data.new_data_shuffle(:, 1);
-% xx = linspace (min(to_plot), max(to_plot), length(to_plot));
-% yy = normpdf (xx, mean(to_plot), std(xx));
-% 
-% F1 = figure;
-% histogram(to_plot, 15, 'Normalization', 'pdf')
-% hold on
-% plot(xx, yy, 'LineWidth', 2)
-
 
 figure;
 for i = 1:work_data.default_result (2)
     subplot (5, 5, i)
+    hist_bars = 12;
+
     to_plot = work_data.new_data_shuffle(:, i);
     xx = linspace (min(to_plot), max(to_plot), length(to_plot));
     yy = normpdf (xx, mean(to_plot), std(xx));
 
-    histogram(work_data.new_data_shuffle(:, i), 10, 'Normalization', 'pdf')
+
+    histogram(work_data.new_data_shuffle(:, i), hist_bars, 'Normalization', 'pdf')
     hold on
     plot(xx, yy, 'LineWidth', 2)
 end
+
+
+
